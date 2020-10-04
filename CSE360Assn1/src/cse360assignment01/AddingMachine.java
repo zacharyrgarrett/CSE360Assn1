b99242062226de1d647c4c088c2a6d3aa71303a2
Zachary <Zachary@DESKTOP-BJ0OGIO>
2020-10-04 15:32:42 -0700

    Initial commit

---
+++ CSE360Assn1/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,31 @@
+package cse360assignment01;
+
+/**
+ * @author Zachary Garrett
+ * file: AddingMachine.java
+ * created: 10/4/2020
+ */
+public class AddingMachine {
+	  private int total;
+	  
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  public void add (int value) {
+	  }
+	
+	  public void subtract (int value) {
+	  }
+	
+	  public String toString () {
+	    return "";
+	  }
+	
+	  public void clear() {
+	  }
+}
